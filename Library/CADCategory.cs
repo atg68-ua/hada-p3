f0882ed419f06fc8dafcb83de72484973292e840
atg68-ua <atg68@alu.ua.es>
2024-03-16 19:13:32 +0100

    23902208X Fixing errors.

--- Library/CADCategory.cs
+++ Library/CADCategory.cs
@@ -58,6 +58,7 @@ namespace Library
                     valores.Add(new ENCategory(reader.GetString(0), reader.GetInt32(1)));
                 }
                 
+                reader.Close();
                 connection.Close();
                 return valores; 
             }
